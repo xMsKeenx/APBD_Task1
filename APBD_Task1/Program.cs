4206ed5861df31259d72aa23cc3f89128092c607
Mskeen <s29920@pjwstk.edu.pl>
2026-03-17 22:36:10 +0100

    Add user input parsing

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace APBD_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers separated by spaces:");
+            string? input = Console.ReadLine();
+
+            string[] parts = input!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = new int[parts.Length];
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                numbers[i] = int.Parse(parts[i]);
+            }
+
+            Console.WriteLine("Numbers loaded successfully.");
+        }
+    }
+}
